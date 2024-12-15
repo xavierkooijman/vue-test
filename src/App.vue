32ca0265f508774e5eef27a6d5466302b014d882
Xavier Kooijman <xavierkooijman@gmail.com>
2024-12-15 22:06:16 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+export default{
+  data(){
+    return{
+      name: "jonh doe",
+      status: "pending",
+      tasks: ["Task1", "Task2", "Task3"],
+      link: "https://google.com"
+    }
+  },
+  methods:{
+    toogleStatus(){
+      if(this.status === "active"){
+        this.status = "pending"
+      }
+      else if(this.status === "pending"){
+        this.status = "inactive"
+      }
+      else{
+        this.status = "active"
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'"> User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a v-bind:href="link">Google</a>
+  <a :href="link">Google</a>
+
+  <button v-on:click="toogleStatus">Change Status</button>
+  <button @click="toogleStatus">Change Status</button>
+</template>
